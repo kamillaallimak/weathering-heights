2fbf49794fb1b7d2b08fce3115beb7fc2f79b12b
Kamilla Al Shadidi Navrotsky <kamilla@alshadidi.se>
2021-06-10 23:35:10 +0200

    integrated api response for forecast done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//-----------------------------------------------  Return Current Day and Time  ------------------------------------------//
+//-----------------------------------------------  Return Current Day and Time (and formate date/time)  ------------------------------------------//
 function showTime(timestamp) {
   let date = new Date(timestamp);
   let hour = date.getHours();
@@ -39,6 +39,15 @@ function showDate(timestamp) {
   return `${numberDate} ${month} ${year}`;
 }
 
+function formatDay(timestamp) {
+  console.log(timestamp);
+  let date = new Date(timestamp * 1000);
+  console.log(date);
+  let dayIndex = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[dayIndex];
+}
+
 //-----------------------------------------------  Unit conversion for Current Temperature ------------------------------------------//
 
 function selectUnit() {
@@ -69,35 +78,44 @@ function changeUnit(e) {
 
 //-----------------------------------------------  Weather for Search Result ------------------------------------------//
 //Get the weather data (for the city or the current lat/long coordinates provided by the API in the previous step)
-function showWeather(position) {
-  document.querySelector("#location").innerHTML = position.data.name;
+function showForecastData(coordinates) {
+  let units = selectUnit();
+  let apiKey = "95aa18dfa2d4f8bc875941518d00ae49";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=${units}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 
-  celsiusTemperature = position.data.main.temp; //send the temperature to the global variable "celsiusTemperature" so that it can be used in other functions as well
+function showWeather(positionResponse) {
+  document.querySelector("#location").innerHTML = positionResponse.data.name;
+
+  celsiusTemperature = positionResponse.data.main.temp; //send the temperature to the global variable "celsiusTemperature" so that it can be used in other functions as well
 
   document.querySelector("#currentTemp").innerHTML =
     Math.round(celsiusTemperature);
 
   document.querySelector("#humidity").innerHTML = Math.round(
-    position.data.main.humidity
+    positionResponse.data.main.humidity
   );
   document.querySelector("#windspeed").innerHTML = Math.round(
-    position.data.wind.speed
+    positionResponse.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
-    position.data.weather[0].description;
+    positionResponse.data.weather[0].description;
 
   document
     .querySelector("#icon")
     .setAttribute(
       "src",
-      `http://openweathermap.org/img/wn/${position.data.weather[0].icon}@2x.png`
+      `http://openweathermap.org/img/wn/${positionResponse.data.weather[0].icon}@2x.png`
     );
 
   let date = document.querySelector("#date");
-  date.innerHTML = showDate(position.data.dt * 1000);
+  date.innerHTML = showDate(positionResponse.data.dt * 1000);
 
   let time = document.querySelector("#time");
-  time.innerHTML = showTime(position.data.dt * 1000);
+  time.innerHTML = showTime(positionResponse.data.dt * 1000);
+  showForecastData(positionResponse.data.coord);
 }
 
 //-----------------------------------------------  Weather - Serach City Input ------------------------------------------//
@@ -132,44 +150,56 @@ function getCurrentPosition(event) {
 }
 
 //-------------------------------------------- Display Weather forecast for the 4 coming days ---------------------------------------------------------//
-function displayForecast() {
+function displayForecast(forecastResponse) {
+  let forecastData = forecastResponse.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
+
   let forecastHTML = "";
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  let firstDay = days.shift();
-  console.log(firstDay);
-  let lastDay = days.pop();
-  console.log(lastDay);
-  console.log(days);
+
   let forecastFirst = `
   <div class="day-first box-a">
              <ul class="ul">
                <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="next day weather icon" class="icons"> 
-               <li>${firstDay}</li>
-               <li > <span class="max-temp">20</span>° <span class="min-temp" ><span>15</span>°</span></li>
+               <li>${formatDay(forecastData[1].dt)}</li>
+               <li > <span class="max-temp">${Math.round(
+                 forecastData[1].temp.max
+               )}</span>° <span class="min-temp" ><span>${Math.round(
+    forecastData[1].temp.min
+  )}</span>°</span></li>
              </ul>
  </div>`;
 
   let forecastMiddle = "";
-  days.forEach(function (day) {
-    forecastMiddle =
-      forecastMiddle +
-      `
-  <div class="day-middle box-b">
-             <ul class="ul">
-               <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="coming days weather icon" class="icons"> 
-               <li id="day-2-weekday">${day}</li>
-               <li > <span class="max-temp">21</span>° <span class="min-temp" ><span>16</span>°</span></li>
-             </ul>
-</div>`;
+  forecastData.forEach(function (day, index) {
+    if (index > 1 && index < 4) {
+      forecastMiddle =
+        forecastMiddle +
+        `
+        <div class="day-middle box-b">
+                  <ul class="ul">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="coming days weather icon" class="icons"> 
+                    <li>${formatDay(day.dt)}</li>
+                    <li> <span class="max-temp">${Math.round(
+                      day.temp.max
+                    )}</span>° <span class="min-temp" ><span>${Math.round(
+          day.temp.min
+        )}</span>°</span></li>
+                  </ul>
+      </div>`;
+    }
   });
 
   let forecastLast = `
    <div class="day-last box-b">
              <ul class="ul">
                <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="last day weather icon" class="icons"> 
-               <li>${lastDay}</li>
-               <li > <span class="max-temp">22</span>° <span class="min-temp" ><span>17</span>°</span></li>
+               <li>${formatDay(forecastData[4].dt)}</li>
+               <li > <span class="max-temp">${Math.round(
+                 forecastData[4].temp.max
+               )}</span>° <span class="min-temp" ><span>${Math.round(
+    forecastData[4].temp.min
+  )}</span>°</span></li>
              </ul>             
   </div>
            `;
@@ -190,5 +220,4 @@ let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", (e) => changeUnit(e));
 
 handleSearchCity("Stockholm");
-displayForecast();
 let celsiusTemperature = null;
