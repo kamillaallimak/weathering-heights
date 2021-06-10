5f0560c2c1aaa69379bdd3734890013e80061c82
Kamilla Al Shadidi Navrotsky <kamilla@alshadidi.se>
2021-06-10 20:25:19 +0200

    forecast style/bootstrap corrected

--- script.js
+++ script.js
@@ -131,7 +131,42 @@ function getCurrentPosition(event) {
   navigator.geolocation.getCurrentPosition(handleCurrentPosition);
 }
 
-//------------------------------------------------------------------------------------------------------------------------//
+//-------------------------------------------- Display Weather forecast for the 4 coming days ---------------------------------------------------------//
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  forecastElement.innerHTML = `
+  <div class="day-1 box-a">
+             <ul class="ul">
+               <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="" id="icon-1" class="icons"> 
+               <li id="day-1-weekday">Fri</li>
+               <li > <span class="max-temp" id="day-1-temp-max">20</span>° <span class="min-temp" ><span id="day-1-temp-min">15</span>°</span></li>
+             </ul>
+ </div>
+  <div class="day-2 box-b">
+             <ul class="ul">
+               <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="" id="icon-1" class="icons"> 
+               <li id="day-2-weekday">Sat</li>
+               <li > <span class="max-temp" id="day-2-temp-max">21</span>° <span class="min-temp" ><span id="day-2-temp-min">16</span>°</span></li>
+             </ul>
+</div>
+<div class="day-3 box-b">
+              <ul class="ul">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="" id="icon-1" class="icons"> 
+               <li id="day-3-weekday">Sun</li>
+               <li > <span class="max-temp" id="day-3-temp-max">20</span>° <span class="min-temp" ><span id="day-3-temp-min">17</span>°</span></li>
+             </ul>            
+   </div>
+   <div class="day-4 box-b">
+             <ul class="ul">
+               <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="" id="icon-1" class="icons"> 
+               <li id="day-4-weekday">Mon</li>
+               <li > <span class="max-temp" id="day-4-temp-max">22</span>° <span class="min-temp" ><span id="day-3-temp-min">17</span>°</span></li>
+             </ul>             
+  </div>
+
+           `;
+}
 
 let searchBtn = document.querySelector("#form");
 searchBtn.addEventListener("submit", getSearchCity);
@@ -146,5 +181,5 @@ let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", (e) => changeUnit(e));
 
 handleSearchCity("Stockholm");
-
+displayForecast();
 let celsiusTemperature = null;
